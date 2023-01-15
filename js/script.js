bb36c90e50171f8c3b3d903d973563b26d4e02ee
Rodrigo Daniel <rodrigoeufrasio2@gmail.com>
2023-01-14 22:08:13 -0300

    LocalStorage Save and Remove

--- js/script.js
+++ js/script.js
@@ -161,6 +161,8 @@ todoForm.addEventListener("submit", (e) => {
         // Delete Notes
     if(targetElement.classList.contains("remove-todo")){
         parentElement.remove();
+
+        removeTodoLocalStorage(todoTitle);
     }
   });
 
@@ -216,10 +218,28 @@ const getTodosLocalStorage = () => {
   return todos;
 };
 
+const loadTodos = () => {
+  const todos = getTodosLocalStorage();
+
+  todos.forEach((todo) => {
+    saveTodo(todo.text, todo.done, 0);
+  });
+};
+
 const saveTodoLocalStorage = (todo) => {
   const todos = getTodosLocalStorage();
 
   todos.push(todo);
 
   localStorage.setItem("todos", JSON.stringify(todos));
-}
+};
+
+const removeTodoLocalStorage = (todoText) => {
+  const todos = getTodosLocalStorage();
+
+  const filteredTodos = todos.filter((todo) => todo.text !== todoText);
+
+  localStorage.setItem("todos", JSON.stringify(filteredTodos));
+};
+
+loadTodos();
